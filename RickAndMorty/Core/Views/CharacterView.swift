4a0e7079bb19ac1fbf18bef5351c7604e69807c7
raressalomie <raressalomie@gmail.com>
2025-11-08 12:29:47 +0100

    modified the character view

--- RickAndMorty/Core/Views/CharacterView.swift
+++ RickAndMorty/Core/Views/CharacterView.swift
@@ -11,20 +11,42 @@ struct CharacterView: View {
     let character: RMCharacter
     
     var body: some View {
-        VStack {
-            AsyncImage(url: URL(string: character.image)) { image in
-                image
-                    .resizable()
-                    .frame(width: 300, height: 300)
-            } placeholder: {
-                Color.red
-                    .frame(width: 300, height: 300)
+        ZStack {
+            VStack(alignment: .center) {
+                AsyncImage(url: URL(string: character.image)) { image in
+                    image
+                        .resizable()
+                        .frame(width: 300, height: 300)
+                } placeholder: {
+                    ProgressView()
+                        .frame(width: 300, height: 300)
+                }
+                .frame(maxWidth: .infinity)
+                
+                VStack(alignment: .leading, spacing: 8) {
+                    Text(character.name)
+                        .font(.title)
+                        .bold()
+                    
+                    Text("Status: " + character.status.rawValue)
+                        .font(.title2)
+                    
+                    Text("Species: " + character.species.capitalized)
+                        .font(.headline)
+                    
+                    Text("Gender: " + character.gender.rawValue)
+                        .font(.headline)
+                    
+                    Text("Origin location: " + character.origin.name)
+                        .font(.headline)
+                    
+                    Text("Current location: " + character.location.name)
+                        .font(.headline)
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.top)
+                .padding(.horizontal)
             }
-            
-            Text(character.name)
-            Text(character.gender.rawValue)
-            Text(character.species)
-            Text(character.status.rawValue)
         }
         .navigationTitle(character.name)
     }
